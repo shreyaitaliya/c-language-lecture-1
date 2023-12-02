cdd7a5c851e754e99a18903e877098270ea13927
shreyaitaliya <shreyaitaliya11@gmail.com>
2023-12-02 15:35:30 +0530

    first commit

---
+++ TASK1.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   clrscr();
+   printf("name :- shreya italiya\n");
+   printf("age :- 21\n");
+   printf("school :- mauni");
+   getch();
+}
---
+++ TASK4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  clrscr();
+  printf("* \n");
+  printf("* \n");
+  printf("* \n");
+  printf("*\t* *\t    * \n");
+  printf("*     *     *      *\n");
+  printf("*    *        *   *\n");
+  printf("*   *          * *\n");
+  printf("*  *\n");
+  printf("* *\n");
+  printf("*");
+  getch();
+}
